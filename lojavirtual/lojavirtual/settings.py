86b5107ccaddceb734c39bd7bffe02afd6e1c08a
erick_braga <@Erickmatheus07>
2021-08-31 07:25:48 -0300

    Adicionar templates e staticos

--- lojavirtual/lojavirtual/settings.py
+++ lojavirtual/lojavirtual/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'lojavirtual.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lojavirtual/main/admin.py
+++ lojavirtual/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Categoria, Produto
 
-# Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Produto)
